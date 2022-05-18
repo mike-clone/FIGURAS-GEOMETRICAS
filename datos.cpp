a1038c6e18d68181315b5173c8bc1b598a86a9e0
Addin <n00215210@upn.pe>
2022-05-18 11:31:50 -0500

    comemtario eliminado

---
+++ datos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+char nombre,apellidos;
+	cout <<"ingresa nombre: ";
+	cin>>nombre;
+	cout<<"ingresa apellidos ";
+	cin>>apellidos;
+	cout<<"\n nombre: "<<nombre<<" apellidos: "<<apellidos<<endl;
+	
+	
+	return 0;
+}
